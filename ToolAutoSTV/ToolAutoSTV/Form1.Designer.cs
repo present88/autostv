16710b070b1489571fbe3432021838716e16adcb
present88 <1851061854@e.tlu.edu.vn>
2020-11-28 21:48:19 +0700

    source code tool autostv viết bới ngôn ngữ c#,by present

---
+++ ToolAutoSTV/ToolAutoSTV/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace ToolAutoSTV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.link1 = new System.Windows.Forms.TextBox();
+            this.link2 = new System.Windows.Forms.TextBox();
+            this.link3 = new System.Windows.Forms.TextBox();
+            this.link4 = new System.Windows.Forms.TextBox();
+            this.tr1 = new System.Windows.Forms.NumericUpDown();
+            this.tr2 = new System.Windows.Forms.NumericUpDown();
+            this.tr3 = new System.Windows.Forms.NumericUpDown();
+            this.tr4 = new System.Windows.Forms.NumericUpDown();
+            this.btcay = new System.Windows.Forms.Button();
+            this.btdangnhap = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.NumericUpDown();
+            this.max = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bao = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tr1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tr2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tr3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tr4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.max)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // link1
+            // 
+            this.link1.Location = new System.Drawing.Point(26, 34);
+            this.link1.Name = "link1";
+            this.link1.Size = new System.Drawing.Size(161, 20);
+            this.link1.TabIndex = 0;
+            // 
+            // link2
+            // 
+            this.link2.Location = new System.Drawing.Point(26, 60);
+            this.link2.Name = "link2";
+            this.link2.Size = new System.Drawing.Size(161, 20);
+            this.link2.TabIndex = 1;
+            // 
+            // link3
+            // 
+            this.link3.Location = new System.Drawing.Point(26, 86);
+            this.link3.Name = "link3";
+            this.link3.Size = new System.Drawing.Size(161, 20);
+            this.link3.TabIndex = 2;
+            // 
+            // link4
+            // 
+            this.link4.Location = new System.Drawing.Point(26, 113);
+            this.link4.Name = "link4";
+            this.link4.Size = new System.Drawing.Size(161, 20);
+            this.link4.TabIndex = 3;
+            // 
+            // tr1
+            // 
+            this.tr1.Location = new System.Drawing.Point(208, 34);
+            this.tr1.Maximum = new decimal(new int[] {
+            8888,
+            0,
+            0,
+            0});
+            this.tr1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tr1.Name = "tr1";
+            this.tr1.Size = new System.Drawing.Size(56, 20);
+            this.tr1.TabIndex = 4;
+            this.tr1.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // tr2
+            // 
+            this.tr2.Location = new System.Drawing.Point(208, 61);
+            this.tr2.Maximum = new decimal(new int[] {
+            8888,
+            0,
+            0,
+            0});
+            this.tr2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tr2.Name = "tr2";
+            this.tr2.Size = new System.Drawing.Size(56, 20);
+            this.tr2.TabIndex = 5;
+            this.tr2.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // tr3
+            // 
+            this.tr3.Location = new System.Drawing.Point(208, 87);
+            this.tr3.Maximum = new decimal(new int[] {
+            8888,
+            0,
+            0,
+            0});
+            this.tr3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tr3.Name = "tr3";
+            this.tr3.Size = new System.Drawing.Size(56, 20);
+            this.tr3.TabIndex = 6;
+            this.tr3.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // tr4
+            // 
+            this.tr4.Location = new System.Drawing.Point(208, 113);
+            this.tr4.Maximum = new decimal(new int[] {
+            8888,
+            0,
+            0,
+            0});
+            this.tr4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tr4.Name = "tr4";
+            this.tr4.Size = new System.Drawing.Size(56, 20);
+            this.tr4.TabIndex = 7;
+            this.tr4.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // btcay
+            // 
+            this.btcay.Location = new System.Drawing.Point(112, 139);
+            this.btcay.Name = "btcay";
+            this.btcay.Size = new System.Drawing.Size(75, 23);
+            this.btcay.TabIndex = 8;
+            this.btcay.Text = "Cày";
+            this.btcay.UseVisualStyleBackColor = true;
+            this.btcay.Click += new System.EventHandler(this.btcay_Click);
+            // 
+            // btdangnhap
+            // 
+            this.btdangnhap.Location = new System.Drawing.Point(26, 139);
+            this.btdangnhap.Name = "btdangnhap";
+            this.btdangnhap.Size = new System.Drawing.Size(80, 23);
+            this.btdangnhap.TabIndex = 9;
+            this.btdangnhap.Text = "Đăng Nhập";
+            this.btdangnhap.UseVisualStyleBackColor = true;
+            this.btdangnhap.Click += new System.EventHandler(this.btdangnhap_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Link Truyện:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Số Chương";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Time Next Chương(Giây):";
+            // 
+            // min
+            // 
+            this.min.Location = new System.Drawing.Point(166, 197);
+            this.min.Minimum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(48, 20);
+            this.min.TabIndex = 13;
+            this.min.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // max
+            // 
+            this.max.Location = new System.Drawing.Point(220, 197);
+            this.max.Minimum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(44, 20);
+            this.max.TabIndex = 14;
+            this.max.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Min:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(217, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Max:";
+            // 
+            // bao
+            // 
+            this.bao.AutoSize = true;
+            this.bao.Location = new System.Drawing.Point(205, 144);
+            this.bao.Name = "bao";
+            this.bao.Size = new System.Drawing.Size(29, 13);
+            this.bao.TabIndex = 17;
+            this.bao.Text = "Bảo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 232);
+            this.Controls.Add(this.bao);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btdangnhap);
+            this.Controls.Add(this.btcay);
+            this.Controls.Add(this.tr4);
+            this.Controls.Add(this.tr3);
+            this.Controls.Add(this.tr2);
+            this.Controls.Add(this.tr1);
+            this.Controls.Add(this.link4);
+            this.Controls.Add(this.link3);
+            this.Controls.Add(this.link2);
+            this.Controls.Add(this.link1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToolAutoSTV";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.tr1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tr2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tr3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tr4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.max)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox link1;
+        private System.Windows.Forms.TextBox link2;
+        private System.Windows.Forms.TextBox link3;
+        private System.Windows.Forms.TextBox link4;
+        private System.Windows.Forms.NumericUpDown tr1;
+        private System.Windows.Forms.NumericUpDown tr2;
+        private System.Windows.Forms.NumericUpDown tr3;
+        private System.Windows.Forms.NumericUpDown tr4;
+        private System.Windows.Forms.Button btcay;
+        private System.Windows.Forms.Button btdangnhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown min;
+        private System.Windows.Forms.NumericUpDown max;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label bao;
+    }
+}
+
---
+++ ToolAutoSTV/ToolAutoSTV/Form1.cs
@@ -0,0 +1,181 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolAutoSTV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        ChromeDriver chrome;
+        Thread thread;
+        Random ran=new Random();
+        int time;
+        private void btdangnhap_Click(object sender, EventArgs e)
+        {
+            if(btdangnhap.Text=="Đăng Nhập")
+            {
+                btdangnhap.Text = "Thoát";
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+                ChromeOptions option = new ChromeOptions();
+                option.AddArgument("user-data-dir=profile");
+                option.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36");
+                chrome = new ChromeDriver(service, option);
+                chrome.Navigate().GoToUrl("http://sangtacviet.com/");
+            }
+            else
+            {
+                try
+                {
+                    thread.Abort();
+                }
+                catch { }
+                try
+                {
+                    chrome.Close();
+                    chrome.Quit();
+                }
+                catch { }                
+                btdangnhap.Text = "Đăng Nhập";
+            }
+        }
+
+        private void btcay_Click(object sender, EventArgs e)
+        {
+            if(btcay.Text=="Cày")
+            {
+                btcay.Text = "Dừng";
+                thread = new Thread(Cay);
+                thread.Start();
+            }
+            else
+            {
+                btcay.Text = "Cày";
+                try
+                {
+                    thread.Abort();
+                }
+                catch { }
+            }
+        }
+        void Cay()
+        {
+            while(true)
+            {             
+                if(link1.Text!="")
+                {
+                    for (int i = 1; i <= (int)tr1.Value; i++)
+                    {
+                        try
+                        {
+                            chrome.Navigate().GoToUrl(link1.Text + i + "/");
+                        }
+                        catch (Exception)
+                        {
+                            Thread.Sleep(TimeSpan.FromSeconds(8));
+                        }
+                        Thread.Sleep(TimeSpan.FromSeconds(8));
+                        NhatBao();                      
+                    }
+                }
+                if (link2.Text != "")
+                {
+                    for (int i = 1; i <= (int)tr2.Value; i++)
+                    {
+                        try
+                        {
+                            chrome.Navigate().GoToUrl(link2.Text + i + "/");
+                        }
+                        catch (Exception)
+                        {
+                            Thread.Sleep(TimeSpan.FromSeconds(8));
+                        }
+                        Thread.Sleep(TimeSpan.FromSeconds(8));
+                        NhatBao();
+                    }
+                }
+                if (link3.Text != "")
+                {
+                    for (int i = 1; i <= (int)tr3.Value; i++)
+                    {
+                        try
+                        {
+                            chrome.Navigate().GoToUrl(link3.Text + i + "/");
+                        }
+                        catch (Exception)
+                        {
+                            Thread.Sleep(TimeSpan.FromSeconds(8));
+                        }
+                        Thread.Sleep(TimeSpan.FromSeconds(8));
+                        NhatBao();
+                    }
+                }
+                if (link4.Text != "")
+                {
+                    for (int i = 1; i <= (int)tr4.Value; i++)
+                    {
+                        try
+                        {
+                            chrome.Navigate().GoToUrl(link4.Text + i + "/");
+                        }
+                        catch (Exception)
+                        {
+                            Thread.Sleep(TimeSpan.FromSeconds(8));
+                        }
+                        Thread.Sleep(TimeSpan.FromSeconds(8));
+                        NhatBao();
+                    }
+                }
+            }
+        }
+        int sl=0;
+        void NhatBao()
+        {
+
+            time = ran.Next((int)min.Value / 9, (int)max.Value / 9);
+            try
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    chrome.ExecuteScript("window.scrollTo( 0, " + i + "*document.body.scrollHeight/9)");
+                    Thread.Sleep(TimeSpan.FromSeconds(time));
+                }
+                chrome.FindElementByXPath("//*[contains(text(),'Nhặt bảo')]").Click();
+                Thread.Sleep(5000);
+                chrome.FindElementByXPath("//*[contains(text(),'Ok')]").Click();
+                Thread.Sleep(5000);
+                sl++;
+                bao.Text = "Bảo:" + sl;
+            }
+            catch { }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                thread.Abort();
+            }
+            catch { }
+            try
+            {
+                chrome.Close();
+                chrome.Quit();
+            }
+            catch { }
+        }
+    }
+}
